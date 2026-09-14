2799fa7bea9c3173bbec10512e35c29ac00a75d5
cairo0116 <cairosaxtenor.0116@gmail.com>
2026-09-14 23:43:55 +0000

    modulo 2

---
+++ fundamentos_ts/dist/etiquetas.js
@@ -0,0 +1,5 @@
+// etiquetas.ts
+const NOMBRES = ["ana", "juan", "pedro", "maria"];
+export function crearEtiqueta(id) {
+    return `Etiqueta: ${NOMBRES[id]}`;
+}
---
+++ fundamentos_ts/src/etiquetas.ts
@@ -0,0 +1,6 @@
+// etiquetas.ts
+const NOMBRES = ["ana", "juan", "pedro", "maria"];
+
+export function crearEtiqueta(id: number) {
+  return `Etiqueta: ${NOMBRES[id]}`;
+}
